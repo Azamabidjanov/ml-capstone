216567ec6099662c2bd84def3f0c19fced23e1e9
Azam Abidjanov <azam.abidjanov@gmail.com>
2019-09-25 09:17:29 -0600

    Set pipenv with pipfile, fixed encoding issues with pullData

--- src/pull-data.py
+++ src/pull-data.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 def PullData():
-	data = pd.DataFrame.from_csv('../data/songDb.tsv', sep='\t')
+	data = pd.read_csv('../data/songDb.tsv', sep='\t', encoding='latin-1')
 	return data
 
 def main():
 	data = PullData()
-	print(data.head())
+	print( data.head() )
 
 if __name__ == "__main__":
 	main()
