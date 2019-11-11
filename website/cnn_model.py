a4b8db1c27d45512a0ebfff7fdb9efa7d28a7e5e
Jay Harrison <jharrison@isengard.mines.edu>
2019-11-11 13:21:00 -0700

    Train/test data is cleaned correctly, model will not compile over SSH. Need further testing on a real machine :)

--- website/cnn_model.py
+++ website/cnn_model.py
@@ -6,18 +6,16 @@ import numpy as np
 import scipy
 from PIL import Image
 import glob
-import matplotlib
-import matplotlib.pyplot as plt
+#import matplotlib
+#import matplotlib.pyplot as plt
 import re
 
-# Fuck
-# Good for testing later
-def clean_data():
+def get_train_data():
     # Load and convert to grayscale
     img_list = []
     img_labels = []
-    for directory in glob.glob('../data/train/*'):
-        for filename in glob.glob(directory + '/*.jpg'):
+    for directory in glob.glob('../data/train/*/'):
+        for filename in glob.glob(directory + '*.jpg'):
             img = np.asarray(Image.open(filename).convert('LA'))
             img_list.append(img)
             
@@ -35,23 +33,44 @@ def clean_data():
                 img_labels.append(4)
             else:
                 img_labels.append(5)
-        print(str(len(img_list)) + ' images converted.')
-        return np.asarray(img_list), np.asarray(img_labels)
+    
+    print(str(len(img_list)) + ' images converted.')
+    return np.asarray(img_list), np.asarray(img_labels)
 
-# Load images and labels
-train_images,train_labels = clean_data()
-x_train = train_images.astype('float32')
-y_train = train_labels.astype('float32')
+def get_test_data():
+    # Load and convert to grayscale
+    img_list = []
+    img_labels = []
+    for filename in glob.glob('../data/test/*.jpg'):
+        img = np.asarray(Image.open(filename).convert('LA'))
+        img_list.append(img)
+            
+        # Find label
+        pic_type = re.search("cardboard|glass|metal|paper|plastic|trash", filename).group()
+        if pic_type == 'cardboard':
+            img_labels.append(0)
+        elif pic_type == 'glass':
+            img_labels.append(1)
+        elif pic_type == 'metal':
+            img_labels.append(2)
+        elif pic_type == 'paper':
+            img_labels.append(3)
+        elif pic_type == 'plastic':
+            img_labels.append(4)
+        else:
+            img_labels.append(5)
+    
+    print(str(len(img_list)) + ' images converted.')
+    return np.asarray(img_list), np.asarray(img_labels)
 
-print(len(x_train))
-
-# Model time
-img_rows, img_cols = 384, 512
-num_classes = 6
-
-input_shape = (img_rows, img_cols, 1)
-
-layers = [
+def get_model(x_train, y_train):
+    # Sizing
+    img_rows, img_cols = 384, 512
+    num_classes = 6
+    input_shape = (img_rows, img_cols, 1)
+    
+    # Model time - CNN
+    layers = [
         Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=input_shape),
         Conv2D(64, kernel_size=(3,3), activation='relu'),
         MaxPool2D(pool_size=(2,2)),
@@ -61,8 +80,28 @@ layers = [
         Dropout(0.2),
         Dense(num_classes, activation='softmax')
         ]
+    
+    # Train and return
+    cnn_model = Sequential(layers)
+    cnn_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    cnn_model.fit(x_train, y_train, epochs=3, validation_split=0.2)
+    print('Model compiled successfully!')
+    return cnn_model
 
-cnn_model = Sequential(layers)
-cnn_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-cnn_model.fit(x_train, y_train, epochs=3, validation_split=0.2)
-print('Model compiled successfully!')
+def main():
+    # Load images and labels
+    train_images,train_labels = get_train_data()
+    test_images,test_labels = get_test_data()
+    x_train = train_images.astype('float32')
+    y_train = train_labels.astype('float32')
+    x_test = test_images.astype('float32')
+    y_test = test_labels.astype('float32')
+
+    # Create our model
+    model = get_model(x_train, y_train)
+    
+    # Test that bitch
+    print('Testing goes here')
+
+if __name__ == '__main__':
+    main()
