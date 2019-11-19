b16f352aee419c8dddb050ef8551b184d6dc6be4
Azam Abidjanov <azam.abidjanov@gmail.com>
2019-11-18 18:47:33 -0700

    Integrated model backend with the website

--- website/flask_basic.py
+++ website/flask_basic.py
@@ -15,11 +15,13 @@ clear_session()
 app = Flask(__name__, template_folder="templates")
 recycle_model = None
 
+material_class = [ 'Cardboard', 'Glass', 'Metal', 'Paper', 'Plastic', 'Trash' ]
+
 def load_recycle_model():
     global recycle_model
     path = Path(os.getcwd())
     #model_file = f'{path}/models/keras-recycle.pth'
-    model_file = '../keras-mnist.h5'
+    model_file = './keras-recycle.h5'
     # Loads from h5 file
     recycle_model = load_model(model_file)
     # build and compile on GPU
@@ -31,18 +33,18 @@ def recycle_predict():
         image =  request.files['file'].read()
         print("Got image")
         # https://stackoverflow.com/a/27537664/818687
-        arr = cv2.imdecode(np.fromstring(image, np.uint8), cv2.IMREAD_UNCHANGED)
+        arr = cv2.imdecode(np.fromstring(image, np.uint8), cv2.IMREAD_GRAYSCALE)
         print("CV2 read image")
         my_image = arr / 255.0
-        my_images = my_image.reshape(1, 28, 28, 1)
+        my_images = my_image.reshape(-1, 384, 512, 1)
         print("Got here")
 
         pred = recycle_model.predict(my_images)
         n = int(np.argmax(pred))
         print(n)
         return jsonify({
-            "message": f"The predicted object for the uploaded image is {n} ",
-            "number": n
+            "message": f"The predicted object for the uploaded image is {material_class[n]} ",
+            "class": material_class[n]
         })
     except Exception as e:
         print(traceback.format_exc())
