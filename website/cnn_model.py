57bade398c911d9e310aa7f40d83ae486467b793
Jay Harrison <jharrison@bb136-03.mines.edu>
2019-11-18 17:41:13 -0700

    Works, acc score of like 30% tho. Don't know too much about that one...

--- website/cnn_model.py
+++ website/cnn_model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Sequential
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 import numpy as np
 import scipy
@@ -16,9 +17,9 @@ def get_train_data():
     img_labels = []
     for directory in glob.glob('../data/train/*/'):
         for filename in glob.glob(directory + '*.jpg'):
-            img = np.asarray(Image.open(filename).convert('LA'))
+            img = np.array(Image.open(filename).convert('L'))
             img_list.append(img)
-            
+
             # Find label
             pic_type = re.search("cardboard|glass|metal|paper|plastic|trash", directory).group()
             if pic_type == 'cardboard':
@@ -35,6 +36,7 @@ def get_train_data():
                 img_labels.append(5)
     
     print(str(len(img_list)) + ' images converted.')
+    #print(np.asarray(img_list).shape)
     return np.asarray(img_list), np.asarray(img_labels)
 
 def get_test_data():
@@ -42,7 +44,7 @@ def get_test_data():
     img_list = []
     img_labels = []
     for filename in glob.glob('../data/test/*.jpg'):
-        img = np.asarray(Image.open(filename).convert('LA'))
+        img = np.asarray(Image.open(filename).convert('L'))
         img_list.append(img)
             
         # Find label
@@ -60,7 +62,7 @@ def get_test_data():
         else:
             img_labels.append(5)
     
-    print(str(len(img_list)) + ' images converted.')
+    print(str(len(img_labels)) + ' images converted.')
     return np.asarray(img_list), np.asarray(img_labels)
 
 def get_model(x_train, y_train):
@@ -71,10 +73,10 @@ def get_model(x_train, y_train):
     
     # Model time - CNN
     layers = [
-        Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=input_shape),
-        Conv2D(64, kernel_size=(3,3), activation='relu'),
-        MaxPool2D(pool_size=(2,2)),
-        Dropout(0.2),
+        Conv2D(32, kernel_size=(3,3), activation='relu', padding='same', input_shape=input_shape),
+        MaxPool2D(),
+        Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'),
+        MaxPool2D(),
         Flatten(),
         Dense(128, activation='relu'),
         Dropout(0.2),
@@ -83,9 +85,9 @@ def get_model(x_train, y_train):
     
     # Train and return
     cnn_model = Sequential(layers)
-    cnn_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    cnn_model.fit(x_train, y_train, epochs=3, validation_split=0.2)
-    print('Model compiled successfully!')
+    cnn_model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    cnn_model.fit(x_train.reshape(-1, img_rows, img_cols, 1), y_train, epochs=5, validation_split=0.2)
+    print('Model trained successfully!')
     return cnn_model
 
 def main():
@@ -96,12 +98,18 @@ def main():
     y_train = train_labels.astype('float32')
     x_test = test_images.astype('float32')
     y_test = test_labels.astype('float32')
+    
+    # TESTING
+    print(test_images.shape)
+    print(x_test.shape)
 
     # Create our model
     model = get_model(x_train, y_train)
     
     # Test that bitch
-    print('Testing goes here')
+    cnn_scores = model.evaluate(x_test.reshape(-1, 384, 512, 1), y_test)
+    print('Accuracy of: ' + str(cnn_scores[1]))
 
 if __name__ == '__main__':
     main()
+
